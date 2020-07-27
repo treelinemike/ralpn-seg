7e7fdc3f0f3d7975324cdae660361c93871b626c
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-07-26 22:40:36 -0400

    first pass at a mask difference

--- nrrd_io_test.m
+++ nrrd_io_test.m
@@ -14,7 +14,11 @@ segColorsSlicer = [ ...
     1.00 0.75 0.00; ... % LK
     0.00 1.00 0.00; ... % RK
     ];
-
+segColorsDiff = [ ...
+    0.00 0.00 0.00; ... % background class
+    0.33 0.33 1.00; ... % Mimics Only
+    1.00 0.33 0.33; ... % Slicer Only
+    ];
 
 % read SLICER data
 headerInfo = nhdr_nrrd_read('C:\Users\f002r5k\Desktop\ralpn_local\slicer-test\kidney_segs_a.nrrd', true);
@@ -31,17 +35,28 @@ figure;
 
 for frameIdx = 1:size(headerInfo.data,3)
     thisFrame = headerInfo.data(:,:,frameIdx);
-    subplot(1,2,1);
+    subplot(1,3,1);
     cla
-%     imshow(thisFrame',[]);  % NOTE THE TRANSPOSE!!!
-    imshow(maskImage(ralpnData2D.image(:,:,frameIdx),thisFrame',segColorsSlicer));  % NOTE THE TRANSPOSE!!!
+    %     imshow(thisFrame',[]);  % NOTE THE TRANSPOSE!!!
+    slicerMask = thisFrame';
+    imshow(maskImage(ralpnData2D.image(:,:,frameIdx),slicerMask,segColorsSlicer));  % NOTE THE TRANSPOSE!!!
 
     title(['\bfFrame ' num2str(frameIdx) ' - Slicer']);
     
-    subplot(1,2,2);
+    subplot(1,3,2);
     cla
-    imshow(maskImage(ralpnData2D.image(:,:,frameIdx),ralpnData2D.label(:,:,frameIdx),segColorsMimics));  % NOTE THE TRANSPOSE!!!
+    mimicsMask = ralpnData2D.label(:,:,frameIdx);
+    imshow(maskImage(ralpnData2D.image(:,:,frameIdx),mimicsMask,segColorsMimics));  % NOTE THE TRANSPOSE!!!
     title(['\bfFrame ' num2str(frameIdx) ' - Mimics']);
+
+    subplot(1,3,3);
+    cla
+    diffMask = double(mimicsMask > 0) - double(slicerMask > 0);
+    diffMask( diffMask == -1 ) = 2;
+    diffMask = uint8(diffMask);
+    imshow(maskImage(ralpnData2D.image(:,:,frameIdx),diffMask,segColorsDiff));  % NOTE THE TRANSPOSE!!!
+    title(['\bfFrame ' num2str(frameIdx) ' - Mask Diff']);
+
     
     drawnow;
 end
